840bd3bc93a33463db2750b647b503302f0ad2c9
birhanmulugeta <birhanmulugeta16@gmail.com>
2023-11-21 10:11:38 +0300

    creating all files

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:ssen_user/services/app_routes.dart';
+import 'package:ssen_user/services/app_theme.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp(
+    appRouter: AppRouter(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.appRouter}) : super(key: key);
+  final AppRouter appRouter;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Synergy Share Exchange Network',
+      theme: AppThemes.appThemeData[AppTheme.lightTheme],
+      home: const MyHomePage(title: 'Synergy Share Exchange Network User'),
+      onGenerateRoute: appRouter.onGenerateRoute,
     );
   }
 }
