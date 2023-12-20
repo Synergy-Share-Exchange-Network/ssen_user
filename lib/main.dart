0e239ec6b877c33ba14871a17b91ea082475617f
wubetayalew <wubetayalewcourse@gmail.com>
2023-12-20 23:42:22 +0300

    desktop responive structre done.

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:ssen_user/Screens/login.dart';
 import 'package:ssen_user/Screens/partial%20screen/purchase.dart';
 import 'package:ssen_user/Screens/signup.dart';
 import 'package:ssen_user/Screens/state%20pages/mainPage/Buttomnavigation.dart';
+import 'package:ssen_user/intermediate.dart';
 import 'package:ssen_user/screens/about.dart';
 import 'package:ssen_user/screens/components/home.dart';
 import 'package:ssen_user/screens/components/subscribers.dart';
+import 'package:ssen_user/screens/desktop_responsive.dart';
 import 'package:ssen_user/screens/setting.dart';
 import 'package:ssen_user/screens/state%20pages/main_page.dart';
 
@@ -47,7 +49,9 @@ class MyApp extends StatelessWidget {
 
       // home: HomePage(),
       // home: const TermAndCondition(),
-      home: MainPage(),
+      // home: MainPage(),
+      // home: DesktopResponsive(),
+      home: Intermediate(),
       // home: PostShareSecondary(),
       // home: const Subscribers(),
       // home: UserProfile(),
